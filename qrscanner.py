5f7eb6b9786d03cd83cb6d5e79aa57a9c2443988
Ashin Roy <ashinroy9961@gmail.com>
2025-09-08 14:01:10 +0530

    added content input fecility

--- qrscanner.py
+++ qrscanner.py
@@ -1,14 +1,16 @@
 import qrcode
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import Tk
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-#this is the code for qrcode generator
-myqr = qrcode.make("https://www.linkedin.com/in/ashin-roy-595375253/")
 
-myqr.save("myqr.png")
+#this is the code for qrcode generator
+url = input("enter the content you want to convert to qr code:")
+
+qrcod = qrcode.make(url)
+qrcod.save("myqr.png")
 
 
 # the code for qr code decoder
-
-QR = decode(Image.open("myqr.png"))
-print(QR[0].data.decode("ascii"))
